e4e8526e42340e4b5d9668f6fe83909d7628c1e5
sohyunadc <72382334+sohyunadc@users.noreply.github.com>
2021-01-21 01:42:21 +0900

    싱글톤 컨테이너 테스트. 참조값이 같은 것 확인.

--- src/test/java/poolparty/core/singleton/SingletonTest.java
+++ src/test/java/poolparty/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package poolparty.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import poolparty.core.AppConfig;
 import poolparty.core.member.MemberService;
 
@@ -53,6 +55,21 @@ public class SingletonTest {
         // isEqualTo -> equal equals
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//      AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
 
